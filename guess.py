489cd81418b6992ab67561cf57bdb317f75852cb
老齐 <qiwsir@gmail.com>
2020-10-27 09:21:09 +0800

    Update guess.py

--- guess.py
+++ guess.py
@@ -4,13 +4,13 @@ import random
 counts = 0
 answer = random.randint(1, 100)
 
-while counts >=0:
+while counts >=0:   # //这个条件总成立，没有起到终止循环的判断作用
     temp = input("请输入一个100以内的正整数： ")
     guess = int(temp)
 
-    if guess > 100:
+    if guess > 100:    # //还可能小于0，或者含有非数字字符
         print("超出范围啦")
-        break
+        break     # 不能随便终止，应该允许用户修改错误
         
     else:
         if guess == answer:
@@ -18,7 +18,7 @@ while counts >=0:
             print("猜中啦！")
             print("你猜了：" + str(counts) + "次")
             break
-        else:
+        else:     # 用多分支，不要嵌套
             if guess < answer:
                 print("小了")
                 counts = counts + 1
